8be68a7f9f29ba7dd53cd3e049f2f42ec98a3f26
Elenhtml <bagslife@yandex.ru>
2018-04-03 03:09:00 +0300

    Часть 2

--- js/stats.js
+++ js/stats.js
@@ -1,15 +1,18 @@
 'use strict';
 
+var CLOUD_X = 150;
+var CLOUD_Y = 220;
 var GAP = 50;
 var BAR_WIDTH = 40;
 var BAR_MAX_HEIGHT = 150;
+var BAR_INITIAL_Y = 130;
 
 window.renderStatistics = function (ctx, NAMES, times) {
-  var canvas = document.getElementById('canvas');
-  ctx = canvas.getContext('2d');
-  //ctx.fillStyle = '#ffffff';
-  //ctx.fillRect(100, 10, 420, 270);
+  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
+  ctx.shadowOffsetX = 10;
+  ctx.shadowOffsetY = 10;
   
+  ctx.fillStyle = '#ffffff';
   ctx.beginPath();
   ctx.moveTo(100, 10);
   ctx.lineTo(520, 10);
@@ -24,33 +27,44 @@ window.renderStatistics = function (ctx, NAMES, times) {
   ctx.moveTo(130, 145);
   ctx.lineTo(100, 10);
   ctx.closePath();
-  ctx.fill('#ffffff');
-  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
-  ctx.shadowOffsetX(10);
-  ctx.shadowOffsetY(10);
+  ctx.stroke();
+  ctx.fill();
   
-  ctx.font = "bold 16px PT Mono";
-  ctx.fillText("Ура вы победили!", 110, 30);
-  ctx.font = "bold 16px PT Mono";
-  ctx.fillText("Список результатов:", 140, 60);
+  ctx.fillStyle = 'black';
+  ctx.font = 'bold 16px PT Mono';
+  ctx.fillText('Ура вы победили!', 230, 30);
+  ctx.font = 'bold 16px PT Mono';
+  ctx.fillText('Список результатов:', 210, 60);
   
   NAMES.unshift = ['Катя'];
   var times = [105, 210, 300, 150];
   
-  var getMaxElement = function (times) {
+  /*var getMaxElement = function (times) {
     var maxElement = times[0];
   
     for (var i = 0; i < times.length; i++) {
-      if (times[i] > maxTime) {
-        maxTime = arr[i];
+      if (times[i] > maxElement) {
+        maxElement = arr[i];
       }
     }
   
-    return maxTime;
+    return maxElement;
   };
   
-  for (var i=0; i < NAMES.length; i++) {
-    ctx.fillRect(BAR_WIDTH + GAP, (BAR_MAX_HEIGHT * times[i]) / maxTime);
-    ctx.fillText(NAMES[i], (BAR_MAX_HEIGHT * times[i]) / maxTime, times[i]);
-  }
+  var maxTime = getMaxElement(times);*/
+  
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  ctx.fillText(times[0], CLOUD_X, 150);
+  ctx.fillRect(CLOUD_X, 180, BAR_WIDTH, 52);
+  ctx.fillText('Катя', CLOUD_X, 260);
+  
+  /*for (var i=0; i < NAMES.length; i++) {
+    ctx.fillStyle = 'blue';
+    if (i=0) {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';  
+    }
+    ctx.fillText(times[i], CLOUD_X + (BAR_WIDTH + GAP)*i, 150);
+    ctx.fillRect(CLOUD_X + (BAR_WIDTH + GAP)*i, BAR_INITIAL_Y + (BAR_MAX_HEIGHT - ((BAR_MAX_HEIGHT * times[i]) / maxTime)), BAR_WIDTH, ((BAR_MAX_HEIGHT * times[i]) / maxTime));
+    ctx.fillText(NAMES[i], CLOUD_X + (BAR_WIDTH + GAP)*i, 260);
+  }*/
 };
