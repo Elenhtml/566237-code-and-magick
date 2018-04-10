d2a1254af40a10b993ca50ccf1f1bafe35cddf9a
Elenhtml <bagslife@yandex.ru>
2018-04-10 15:10:46 +0300

    Выполнено задание #12 по учебному проекту

---
+++ js/setup.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var userShow = document.querySelector('.setup');
+userShow.classList.remove('hidden');
+
+var heroNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var heroSurNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var chooseRandom = function (arr) {
+  var choice = arr[Math.floor(Math.random() * arr.length)];
+  return choice;
+};
+
+var massHeroes = [
+  {
+    name: chooseRandom(heroNames) + ' ' + chooseRandom(heroSurNames),
+    coatColor: chooseRandom(coatColors),
+    eyesColor: chooseRandom(eyesColors)
+  },
+  {
+    name: chooseRandom(heroNames) + ' ' + chooseRandom(heroSurNames),
+    coatColor: chooseRandom(coatColors),
+    eyesColor: chooseRandom(eyesColors)
+  },
+  {
+    name: chooseRandom(heroNames) + ' ' + chooseRandom(heroSurNames),
+    coatColor: chooseRandom(coatColors),
+    eyesColor: chooseRandom(eyesColors)
+  },
+  {
+    name: chooseRandom(heroNames) + ' ' + chooseRandom(heroSurNames),
+    coatColor: chooseRandom(coatColors),
+    eyesColor: chooseRandom(eyesColors)
+  }
+];
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+var createWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i=0; i < massHeroes.length; i++) {
+  fragment.appendChild(createWizard(massHeroes[i]));
+}
+similarListElement.appendChild(fragment);
+userShow.querySelector('.setup-similar').classList.remove('hidden');
